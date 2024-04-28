cbe6da869dd7a8e26d9377bfefcf23ef76da1430
liyanzhuo2022 <654024083@qq.com>
2024-04-28 16:21:28 +0800

    2nd commit, modify TestOffByOne

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -12,8 +12,11 @@ public class TestOffByOne {
     public void testEqualChars() {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars('A', 'B'));
+        assertTrue(offByOne.equalChars('&', '%'));
         assertFalse(offByOne.equalChars('a', 'a'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('a', 'B'));
     }
 }
